7f635b4bc1f5da84b699b3e28665ffd8595c316c
Damian Phung <damianphung@gmail.com>
2020-08-21 14:42:38 +1000

    add dashboaprd

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import hasuraDataProvider from "ra-data-hasura";
 //import UserIcon from '@material-ui/icons/Group';
 import { FirebaseAuthProvider } from "react-admin-firebase";
 import CustomLoginPage from './CustomLoginPage';
+import Dashboard from './Dashboard';
+
 import firebase from 'firebase';
 // Define Firebase auth provider
 const firebaseConfig = {
@@ -24,7 +26,7 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const firebaseOptions = {
   // Enable logging of react-admin-firebase
-  app: firebaseApp,
+  // app: firebaseApp,
   logging: true,
   // Authentication persistence, defaults to 'session', options are 'session' | 'local' | 'none'
   persistence: "session"
@@ -32,15 +34,46 @@ const firebaseOptions = {
 
 // This defines the AuthProvider first
 const fbAuthProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
+console.log("auth provider --> ", fbAuthProvider);
+
+// Use this just like the normal auth provider
+const myAuthProvider = {
+  // Copy all authprovider functionality
+  ...fbAuthProvider,
+  // Wrap the login and check for custom claims
+  login: async (params) => {
+    // const claims = await fbAuthProvider.getPermissions();
+
+    // console.log("Login claims -> ", claims);    
+    const user = await fbAuthProvider.login(params);
+    // getPermissions is how when get the custom claims for the logged in user
+
+    // const isAdmin = Array.isArray(claims) && claims.includes("admin");
+    return user;
+    // if (isAdmin) {
+    //   return user;
+    // }
+    // // Make sure user is logged out, if not an admin
+    // await fbAuthProvider.logout()
+    // throw new Error("Login error, invalid permissions");
+  },
+};
+
+// console.log("myAuthProvider provider --> ", myAuthProvider);
+
+
+
 
 // Create a client for Hasura with the right headers
 const httpClient = (url, options = {}) => {
-    return fbAuthProvider.getJWTToken().then(function (JWT){
+    return myAuthProvider.getJWTToken().then(function (JWT){
       if (!options.headers) {
           options.headers = new Headers({ Accept: 'application/json' });
       }
       // add your own headers here
-      console.log("JWT -> ", JWT);
+      // console.log("JWT -> ", JWT);
+
+      // console.log("fbAuthProvider.getPermissions() -> ", fbAuthProvider.getPermissions()); 
       options.headers.set('Authorization', `Bearer ${JWT}`);
       return fetchUtils.fetchJson(url, options);
     });
@@ -54,17 +87,20 @@ const dataProvider = hasuraDataProvider('https://hasura-container-test.herokuapp
 const App = () => {
   return (
     <Admin
+      dashboard={Dashboard}
+      authProvider={myAuthProvider}
       dataProvider={dataProvider}
-      authProvider={fbAuthProvider}
       loginPage={CustomLoginPage}
     >
-      <Resource
-        name="todos"
-        list={TodoList}
-        edit={TodoEdit}
-        create={TodoCreate}
-      />
-      <Resource name="users" list={UserList} show={UserShow} />
+      {(permissions) => [
+        <Resource
+          name="todos"
+          list={TodoList}
+          edit={TodoEdit}
+          create={TodoCreate}
+        />,
+        <Resource name="users" list={UserList} show={UserShow} />
+      ]}
     </Admin>
   );
 };
--- src/CustomLoginPage.js
+++ src/CustomLoginPage.js
@@ -3,38 +3,48 @@ import React from "react";
 import { Login, LoginForm } from "react-admin";
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
+import { Typography } from '@material-ui/core';
+// import { ThemeProvider , MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 
 // Configure FirebaseUI.
 const uiConfig = {
   // Popup signin flow rather than redirect flow.
   signInFlow: 'popup',
   // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '#/',
+  signInSuccessUrl: '/',
   // We will display Google and Facebook as auth providers.
   signInOptions: [
-    // {
-    //     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-    //     signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
-    //     // requireDisplayName: true
-    // },    
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+        requireDisplayName: false
+    },    
+    // firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-  ]
+  ],
+  // callbacks: {
+  //   // Avoid redirects after sign-in
+  //   signInSuccessWithAuthResult: () => false
+  // }     
 };
 
 const SignInScreen = () => <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>;
 
-const CustomLoginForm = props => (
-  <div>
-    <div style={{fontFamily: "monospace", marginLeft: '15px'}}>
-      <p>Username: test@example.com</p>
-      <p>Password: password</p>
-    </div>
-    <LoginForm {...props} />
-    <SignInScreen />
-  </div>
-);
+const CustomLoginForm = props => {
+  return(
+      <div>
+        <div style={{fontFamily: "monospace", marginLeft: '5em', marginRight: '5em' }}>
+          <Typography align="centre"> Welcome to xxxx </Typography>
+        </div>
+        {/* <LoginForm {...props} /> */}
+        <SignInScreen />
+      </div>    
+
+  );
+
+};
 
 const CustomLoginPage = props => (
   <Login {...props}>
