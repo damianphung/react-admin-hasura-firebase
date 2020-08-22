1ca792239ca287b4cb4a5720f2695bcb31e08d51
Damian Phung <damianphung@gmail.com>
2020-08-22 15:45:02 +1000

    Fix initial sign on without custom hasura claims

--- src/App.js
+++ src/App.js
@@ -3,13 +3,10 @@ import { fetchUtils, Admin, Resource } from "react-admin";
 import { TodoCreate, TodoEdit, TodoList } from "./todos";
 import { UserList, UserShow } from "./users";
 import hasuraDataProvider from "ra-data-hasura";
-//import PostIcon from '@material-ui/icons/Book';
-//import UserIcon from '@material-ui/icons/Group';
 import { FirebaseAuthProvider } from "react-admin-firebase";
 import CustomLoginPage from './CustomLoginPage';
 import Dashboard from './Dashboard';
-
-// import firebase from 'firebase';
+import firebase from 'firebase';
 // Define Firebase auth provider
 const firebaseConfig = {
   apiKey: "AIzaSyB8CXiCfujjLdry1m35Zb-fpAhUirjsqoo",
@@ -22,65 +19,65 @@ const firebaseConfig = {
 };
 
 
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const firebaseOptions = {
   // Enable logging of react-admin-firebase
-  // app: firebaseApp,
+  app: firebaseApp,
   logging: true,
   // Authentication persistence, defaults to 'session', options are 'session' | 'local' | 'none'
   persistence: "session"
 };
 
 // This defines the AuthProvider first
-const fbAuthProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
-console.log("auth provider --> ", fbAuthProvider);
+const baseAuthProvider = FirebaseAuthProvider(firebaseConfig, firebaseOptions);
 
 // Use this just like the normal auth provider
 const myAuthProvider = {
   // Copy all authprovider functionality
-  ...fbAuthProvider,
+  ...baseAuthProvider,
   // Wrap the login and check for custom claims
-  login: async (params) => {
-    // const claims = await fbAuthProvider.getPermissions();
-
-    // console.log("Login claims -> ", claims);    
-    const user = await fbAuthProvider.login(params);
-    // getPermissions is how when get the custom claims for the logged in user
-
-    // const isAdmin = Array.isArray(claims) && claims.includes("admin");
-    return user;
-    // if (isAdmin) {
-    //   return user;
-    // }
-    // // Make sure user is logged out, if not an admin
-    // await fbAuthProvider.logout()
-    // throw new Error("Login error, invalid permissions");
+  getPermissions: async (params) => {
+      const permissions = await baseAuthProvider.getPermissions();
+      console.log("permissions called with params -> ", params);
+      if( !permissions["https://hasura.io/jwt/claims"] )
+      {
+        // from 
+        // https://firebase.google.com/docs/auth/admin/custom-claims
+        const user = await baseAuthProvider.checkAuth();
+        if (user) {
+          // Check if refresh is required.
+          let metadataRef = null;
+          let callback = null;
+          metadataRef = firebase.database().ref('metadata/' + user.uid + '/refreshTime');
+          callback = (snapshot) => {
+            // Force refresh to pick up the latest custom claims changes.
+            // Note this is always triggered on first call. Further optimization could be
+            // added to avoid the initial trigger when the token is issued and already contains
+            // the latest claims.
+            user.getIdToken(true);
+          };
+          // Subscribe new listener to changes on that node.
+          metadataRef.on('value', callback);
+        }      
+      }   
+      return permissions;
   },
 };
 
-// console.log("myAuthProvider provider --> ", myAuthProvider);
-
-
-
-
 // Create a client for Hasura with the right headers
-const httpClient = (url, options = {}) => {
+const httpClient = (url, options = {}) =>  {
     return myAuthProvider.getJWTToken().then(function (JWT){
       if (!options.headers) {
           options.headers = new Headers({ Accept: 'application/json' });
       }
-      // add your own headers here
-      // console.log("JWT -> ", JWT);
 
-      // console.log("fbAuthProvider.getPermissions() -> ", fbAuthProvider.getPermissions()); 
       options.headers.set('Authorization', `Bearer ${JWT}`);
       return fetchUtils.fetchJson(url, options);
     });
-    };
+};
 
 // Define the dataprovider
-// ngtok this and deploy on firebase
 const dataProvider = hasuraDataProvider('https://hasura-container-test.herokuapp.com', httpClient);
 
 // Define main App
@@ -92,15 +89,13 @@ const App = () => {
       dataProvider={dataProvider}
       loginPage={CustomLoginPage}
     >
-      {(permissions) => [
         <Resource
           name="todos"
           list={TodoList}
           edit={TodoEdit}
           create={TodoCreate}
-        />,
+        />
         <Resource name="users" list={UserList} show={UserShow} />
-      ]}
     </Admin>
   );
 };
